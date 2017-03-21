6fb408b8ddadf9bb1811cbe88920864d3f5604af
Justin Valentin <Justinvalentine88@gmail.com>
2017-03-20 23:53:24 -0400

    tried my best to get adventure mode but couldnt figure the missing pieces.

--- app.rb
+++ app.rb
@@ -1,22 +1,16 @@
-puts "What is the first number?"
-first_number = gets.chomp.to_f
+puts "enter as many numbers as you'd like, press enter when you're done"
+array = [ ]
+input = gets.chomp
+while
+  input != ""
+  array.push input
+  input = gets.chomp
+end
 
-puts "What is the second number?"
-second_number = gets.chomp.to_f
+total =
 
-puts "What is the third number?"
-third_number = gets.chomp.to_f
+average = input.sum / input.size.to_f
 
-puts "what is the fourth number?"
-fourth_number = gets.chomp.to_f
+puts total
 
-puts "what is the fifth number?"
-fifth_number = gets.chomp.to_f
-
-total = first_number + second_number + third_number + fourth_number + fifth_number
-
-average_num = total / 5
-
-puts "the total is #{total}"
-
-puts "the average is #{average_num}"
+puts average
